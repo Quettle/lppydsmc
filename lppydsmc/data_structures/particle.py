39afef22e4d0ddfb4c8b5b51d3be972174d5ae20
PaulCalot <paul.calot@hotmail.fr>
2021-06-08 15:02:13 +0200

    Made Particle derive from Container

--- lppydsmc/data_structures/particle.py
+++ lppydsmc/data_structures/particle.py
@@ -1,6 +1,8 @@
+from .container import Container
+
 import numpy as np
 
-class Particle(object):
+class Particle(Container):
     """Container for all particles of a given type. Uses the hard sphere model. 
 
     Return a container with method [...].
@@ -19,11 +21,10 @@ class Particle(object):
             radius (float): the radius of the particle
             size_array (int): max size of the array (memory is allocated before the simulation for performance arguments)
         """
-        self.size_array = size_array
-        self.arr = np.empty(shape = (size_array, 5), dtype = float)
-        self.current = 0
-        self.part_type = part_type
+        Container.__init__(self, size_array, number_of_elements=5, dtype = float)
 
+        # specific to particles handler
+        self.part_type = part_type
         self.params = self._init_params(charge, mass, radius)
 
     def _init_params(self, charge, mass, radius):
@@ -33,73 +34,7 @@ class Particle(object):
     def _compute_cross_section(self, radius):
         return np.pi*4*(radius)**2
 
-    # -------------------- Updating the list -------------------- #
-    def add(self, o): # pos must be a tuple
-        self.arr[self.current] = o
-        self.current+=1
-
-    def add_multiple(self, o):
-        self.arr[self.current:self.current+o.shape[0]] = o[:]
-        self.current+=o.shape[0]
-    
-    # we will have the same issue of not being able to use it I think.
-    # @njit # in the long run, we may want to use 
-    def delete_multiple(self, idxes):
-        # best is Numba - however we can not use sort('stable') (idxes is almost sorted)
-        
-        # Previous version - copying array takes lots of time for very big arrs
-        # we could use np.delete() however it changes the size of the return arrays so we have to be more careful
-        # self.arr[:self.size_array-idxes.shape[0],:] = np.delete(self.arr, idxes, axis = 0) # operation is not inplace
-        # self.current-=idxes.shape[0]
-        
-        idxes.sort() # inplace
-        # forced to loop 
-        for idx in np.flip(idxes): # view = constant time 
-            self.arr[idx] = self.arr[self.current-1]
-            self.current -= 1
-
-    def delete(self, idx):
-        """Removes the element at index *idx*.
-
-        Args:
-            idx (int): index of the element to be removed
-        """
-        self.arr[idx] = self.arr[self.current-1]
-        self.current -= 1
-    
-    def pop(self, idx):
-        """Removes the element at index *idx* and returns it.
-
-        Args:
-            idx (int): index of the element to be removed
-
-        Returns:
-            ndarray: the removed element
-        """
-        tmp = np.copy(self.arr[idx])
-        self.arr[idx] = self.arr[self.current-1] # the last one is moved before
-        self.current -= 1
-        return tmp
-
-    def remove(self, o):
-        """Removes the first element of the list corresponding to o (by reference, not value).
-
-        Args:
-            o (ndarray): the array to be removed.
-        """
-        for idx in range(self.current):
-            if(self.arr[idx] == o): # same object # np.array_equal(self.arr[idx], o) => same values
-                self.arr[idx] = self.arr[self.current-1]
-                self.current -= 1
-                break
-
     # --------------------- Getter and Setter ------------------- #
 
     def get_params(self):
         return self.params
-    
-    def get_current(self):
-        return self.current
-
-    def get_particles(self):
-        return self.arr[:self.current]
