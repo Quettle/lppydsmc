2dac097e5c2df79160b8abac6611e501190fac2f
PaulCalot <paul.calot@hotmail.fr>
2021-06-10 14:55:16 +0200

    Corrected some bugs and tested on a simple system.

--- lppydsmc/poisson_solver/solver.py
+++ lppydsmc/poisson_solver/solver.py
@@ -34,13 +34,23 @@ def solver(mesh, boundary_conditions, charge_density):
                         'k_0' : 1.0,
                         'k_1' : 0.01,
                         'tol' : 1e-14
-                    }
+                    },
                     'degree' : 0,
                     'boundary' : 'x[1] <= 0.5 + tol ? true : false',
                     'boundary_kwargs' : {
                         'tol' : 1e-14
                     }
                 }
+            }
+
+            charge_density = {
+                'value' : 'max_density-x[0]*(max_density-min_density)/lx',  # must be a string too
+                'degree' : 2,
+                'kwargs' : {
+                    'max_density' : 2e17,
+                    'min_density' : 1e17,
+                    'lx' : 1.0
+                } 
             }
     Returns:
         (): returns the potential (scalar) and electrical (vector) fields.
@@ -50,19 +60,23 @@ def solver(mesh, boundary_conditions, charge_density):
     # boundaries_fn must return a boolean value - True is the point x lies on the boundary 
 
     # Defining the finite element function space
-    V = FunctionSpace(mesh = mesh, 'Lagrange', degree = 1)
-    
+    V = FunctionSpace(mesh, 'Lagrange', 1,  1) # mesh, family, degree
     # Defining boundary conditions 
     bc_list = []
-    for key, value in boundary_conditions.item():
+    for key, value in boundary_conditions.items():
         # creating boundary condition (only dirichlet), by default Fenics takes Neumann. Robin not (yet) taken into account.
-        if(boundary_conditions['type'] == 'Dirichlet'):
+        if(value['type'] == 'Dirichlet'):
             # solution on the boundary
-            u_d = Expression(boundary_conditions['solution'], degree=boundary_conditions['deg'], **boundary_conditions['solution_kwargs']) # degree should be at least 1 - better too for more accuracy
-            bc_list.append(DirichletBC(V, u_d, boundary_conditions['boundary']), **boundary_conditions['boundary_kwargs']) # boundary defines a subdomain of the main domain - and it is a string
+            u_d = Expression(value['solution'], degree=value['degree'], **value['solution_kwargs']) # degree should be at least 1 - better too for more accuracy
+            # sub_domain = CompiledSubDomain(value['boundary'], **value['boundary_kwargs'])
+            # temporary solution :
+            cmd  = value['boundary']
+            for key, val in value['boundary_kwargs'].items():
+                cmd = cmd.replace(key, str(val))
+            bc_list.append(DirichletBC(V, u_d, cmd)) #  , **value['boundary_kwargs'] - boundary defines a subdomain of the main domain - and it is a string
 
     # Defining source term
-    charge_density = Expression('{}/VACUUM_PERMITTIVITY'.format(charge_density['rho']), degree=charge_density['deg'], VACUUM_PERMITTIVITY = VACUUM_PERMITTIVITY) # Note : optimization can be made in case the source_term is constant
+    charge_density = Expression('{}/VACUUM_PERMITTIVITY'.format(charge_density['value']), degree=charge_density['degree'], VACUUM_PERMITTIVITY = VACUUM_PERMITTIVITY, **charge_density['kwargs']) # Note : optimization can be made in case the source_term is constant
 
     # Defining the variational problem
     u = TrialFunction(V) # Trial function
