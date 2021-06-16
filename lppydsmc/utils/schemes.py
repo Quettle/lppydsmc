a48bf89204d98316488b2c8c9f981a58d66c3e36
PaulCalot <paul.calot@hotmail.fr>
2021-06-16 14:26:51 +0200

    Change euler explicit scheme so it considers the function f returns the derivative of the input array.

--- lppydsmc/utils/schemes.py
+++ lppydsmc/utils/schemes.py
@@ -7,9 +7,9 @@ import numpy as np
 # The user should be careful to make *f* as efficient as possible (and vectorize it and use only numpy).
 
 def euler_explicit(arr, f, dt, t, args):
-    acc = f(arr, t, *args) # acc is 3D [ax, ay, az]
-    arr[:, 2:] += dt*acc
-    arr[:, :2] += arr[:, 2:4]*dt
+    der = f(arr, t, *args) # acc is 3D [ax, ay, az]
+    arr[:, :] += dt*der
+    # arr[:, :2] += arr[:, 2:4]*dt
     return arr
 
 # problem here : 
@@ -17,12 +17,11 @@ def euler_explicit(arr, f, dt, t, args):
 # we need the acceleration of the previous step too. However we don't know how to compute that
 # so what we do is : returning arr and the acceleration so we can save it for the next time
 # acc(n-1) should be saved in *args*
-def leap_frog(arr, f, dt, t, args):
-    acc = f(arr, t, *args) # acc is 3D [ax, ay, az], acc = a(i), a(i+1)
-    arr[:,:2] += arr[:, 2:4]*dt + 0.5*acc[0, :2]*dt*dt # pos(i+1) = pos(i) + v(i)*dt + 1/2 a(i) * dt^2
-    arr[:, 2:] += 0.5*(acc[0]+acc[1])*dt # v(i+1) = v(i) + 1/2 (a(i)+a(i+1))*dt
-    return arr, acc
-
+# def leap_frog(arr, f, dt, t, args):
+#     der = f(arr, t, *args) # acc is 3D [ax, ay, az], acc = a(i), a(i+1)
+#     arr[:,:2] += arr[:, 2:4]*dt + 0.5*der[0, 2:4]*dt*dt # pos(i+1) = pos(i) + v(i)*dt + 1/2 a(i) * dt^2
+#     arr[:, 2:] += 0.5*(acc[0]+acc[1])*dt # v(i+1) = v(i) + 1/2 (a(i)+a(i+1))*dt
+#     return arr, acc
 
 def rk4(arr, f, dt, t, args): 
     k1 = f(arr, t, *args) # 5D [vx, vy, ax, ay, az] - vz is useless as there is nothing to update
